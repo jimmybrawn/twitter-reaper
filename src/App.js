cb155fccf889be75f390869fd2e59c128eb72c84
Jimmy Brawn <jimmy.brawn@zooma.se>
2017-10-07 01:27:20 +0200

    setup navigation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Navbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#">Twitter Reaper</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">How it works</NavItem>
+            <NavItem eventKey={2} href="#">About</NavItem>
+            <NavDropdown eventKey={3} title="Reaper" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Harvest Followers</MenuItem>
+              <MenuItem eventKey={3.2}>Send DM's</MenuItem>
+              <MenuItem eventKey={3.3}>Tell your friends</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Settings</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      
+          <h1 className="App-title">Twitter Reaper reaps what you sow</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="App-intro container">
+          The purpose of this app is to collect all your followers that don't follow you back. You can easily unfollow them or send them a DM to suggest to follow you back.
         </p>
       </div>
     );
