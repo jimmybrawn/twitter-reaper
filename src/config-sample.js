ed1f9718826f876e509b8ee81c5849e2f224b945
Jimmy Brawn <jimmy.brawn@zooma.se>
2017-10-08 00:55:44 +0200

    config file

---
+++ src/config-sample.js
@@ -0,0 +1,10 @@
+/*
+This is a sample file, provide the real data and save as config.js
+*/
+
+const settings = {
+    apiKey: 'YOUR_API_KEY',
+    apiKeySecret: 'YOUR_SECRET_API_KEY'
+}
+
+export default Config
