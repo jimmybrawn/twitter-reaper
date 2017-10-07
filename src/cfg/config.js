4ff78fa592f825cc9d16b3cbd60a952b3eb0c7d8
Jermaine de Bruyne <jimmybrawn@users.noreply.github.com>
2017-10-08 01:44:33 +0200

    Delete config.js

--- src/cfg/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    apiKey:             '5XYNHG6UnX8jgTfH8Zq3y3vYr',
-    apiKeySecret:       'zC0PLUSbTqX5dL9kJq3Rzisi2HAY0C2hRPtcR2SHNuAvkSsp8X',
-    accessToken:        '837654181808844800-x3OlXMmi7Cj1mOpzIdpKGMu3Zep18uU',
-    accessTokenSecret:  'VeHc4lpNmelXt9zaC7InIH3FSSwEo0kfvSVCAyroK9WGg'
-}
